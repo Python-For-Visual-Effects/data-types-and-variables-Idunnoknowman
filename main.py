75944928a4925f5e525055ae79d87485e255e94d
Idunnoknowman <66569061+Idunnoknowman@users.noreply.github.com>
2020-10-03 13:15:36 -0400

    all answers

--- main.py
+++ main.py
@@ -16,12 +16,23 @@ number_2= 32
 print(number_1+number_2)
 
 # 3.- Make a program that prints a sentence that includes at least 3 variables.
+greeting= "Hello"
+first_name= "Hussain"
+last_name= "Dabhiya"
 
+print(greeting + " my name is " + first_name +" "+ last_name)
 
 # 4.- Given a sentence, assign the string to a variable then print the number of 
 # characters in the sentence. 
 # The sentence is "This is my first Python program."
 
+psentence= "This is my first Python program."
+print( len (psentence))
+
 # 5.- Given the resolution 1920 x 1080, make a program that prints a string with 
 # the 10% over-scan value of those numbers. The printed string must be as 
 # follows: "The 10% overscan of 1920 is <value 1>, and the 1080 is <value 2>"
+
+value1= 192
+value2= 108
+print("The 10% overscan of 1920 is " + str(value1) + ", and the 1080 is " + str(value2) + ".")
